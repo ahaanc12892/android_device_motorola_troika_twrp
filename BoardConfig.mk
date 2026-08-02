a941759264862ad97976dd6cfd5fc65571bbb2e1
Ahaan <ahaanc12892@users.noreply.github.com>
2026-08-02 12:57:53 +0530

    Explicit TARGET_SUPPORTS_64/32_BIT_APPS (A14 board_config requirement)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,11 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# ABI support flags (required by A14 board_config checks)
+TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_SUPPORTS_32_BIT_APPS := true
+TARGET_USES_64_BIT_BINDER := true
+
 # CPUSets
 ENABLE_CPUSETS := true
 ENABLE_SCHEDBOOST := true
